dcad1118d36aa661d77a3ce71071664e8a3863ac
Anuththa <157110151+Anuththa@users.noreply.github.com>
2024-07-25 17:26:14 +0530

    Update public.client.js

--- Client/src/api/client/public.client.js
+++ Client/src/api/client/public.client.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import queryString from "query-string";
 
-const baseURL = "https://movie-beryl-psi.vercel.app/api/v1/";
+const baseURL = "https://https://movieapi.vercel.app//api/v1/";
 
 const publicClient = axios.create({
   baseURL,
