449bf2df988000d9ef6e081489c090bb343ee1b4
Dinuka <dinukarathnayake20@gmail.com>
2024-07-29 13:57:43 +0530

    commit 5

--- Client/src/api/client/private.client.js
+++ Client/src/api/client/private.client.js
@@ -1,11 +1,9 @@
 import axios from "axios";
 import queryString from "query-string";
 
-<<<<<<< HEAD
+
 const baseURL = "https://moonflix-api.vercel.app/api/v1/";
-=======
-const baseURL = "https://https://moonflix-api.vercel.app/api/v1/";
->>>>>>> a01b2056a416af1e2ebebb31db8460a7e0fdeb5f
+
 
 const privateClient = axios.create({
   baseURL,
